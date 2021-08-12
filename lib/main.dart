cd0620d4f110557342353d9ccc634dabee06c6b7
AMELKHdfjd <ia_khamoum@esi.dz>
2021-08-12 10:31:34 +0200

    adding the refresh state

--- lib/main.dart
+++ lib/main.dart
@@ -3,50 +3,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const  MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget{
+const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
+@override
+_helloFlutterStateApp createState() => _helloFlutterStateApp();
+ 
+
+}
+String english = "Salut Amel !";
+String spanish = "coucou";
+
+class _helloFlutterStateApp extends State<MyApp>{
+  //Dephault greeting is in english
+String displaytext = english;
+@override
+Widget build(BuildContext context){
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("hello flutter app"),
+        leading: Icon(Icons.home),
+        actions : <Widget>[
+          IconButton(
+         
+             icon: Icon(Icons.refresh),
+                onPressed: (){
+                  setState(() {
+                    displaytext =
+                    displaytext == english ?
+                        spanish
+                      : english;
+                  });
+                },
+          )
+        ],
+        
+        ),
+      body: Center(
+        child: Text(
+          displaytext,
+          style:TextStyle(fontSize: 30),
+        ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("hello flutter app"),
-          leading: Icon(Icons.home),
-          actions: <Widget>[
-            IconButton(onPressed  :  () {}, icon: Icon(Icons.refresh))
-          ],
+    
+    ),
 
+  );
 
-          ),
-          body : Center(
-            child: Text('hello amel !',
-            style: TextStyle(fontSize: 25),
-            ),
-            )
+}
 
 
 
-      )
-    );
-  }
 }
 
 
